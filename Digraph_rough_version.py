a61d246f2036c7fb9548a56966732a8c70bdf0e1
Sphiwe Kunene <ksphiwe2@gmail.com>
2020-08-15 00:24:20 +0200

    Digraph_rough_version.py

--- Digraph_rough_version.py
+++ Digraph_rough_version.py
@@ -5,11 +5,11 @@ df=pd.read_excel('Connections_history_em.xlsx')
 dot = Digraph()
 
 
-
+colors=df['Colors']
 for index, row in df.iterrows():
- dot.edge(row["Influencer"], row["Influencee"], label='')
+ dot.edge(row["Influencer"], row["Influencee"],label='',color=row["Colors"])
 
-
+#dot.attr(color='green')
 
 dot.render('Digraph_rough_version.pdf', view=True)
 
